b1cae87a1ff8ce59721d9d9199d746be17653b53
MartyMcAir <oth-corp@yandex.ru>
2020-06-28 13:13:29 +0300

    add xml config

--- JRushBofTheB/src/z_Thread_BiG_Pack/Tmp.java
+++ JRushBofTheB/src/z_Thread_BiG_Pack/Tmp.java
@@ -1,13 +1,7 @@
 package z_Thread_BiG_Pack;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Semaphore;
-import java.util.concurrent.TimeUnit;
-import java.util.stream.IntStream;
-
 public class Tmp {
     public static void main(String[] args) {
-
+        System.out.println("test");
     }
 }
